fac137280b73d9688d562ca565b2deaec1f80f46
huyton <huyton2203@gmail.com>
2024-12-13 14:56:35 +0700

    init develop

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/', (req, res) => {
+    res.send('it works')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`)
+})
