05b555de8d16a6452b0ab94d3e4c5b77c877e627
ltn1006 <134504975+ltn1006@users.noreply.github.com>
2025-01-10 17:22:43 +0700

    Add files via upload
    Thêm ProductDetailView
    Chỉnh sửa HomeView, SearchView, ShopView -> dẫn đến ProductDetailView

---
+++ client/ShopView.vue
@@ -0,0 +1,170 @@
+<template>
+    <div>
+        <div class="row bg-tint my-3 py-3 align-items-center">
+            <div class="col-md-6 col">
+                <div class="d-flex align-items-center">
+                    <div class="d-flex me-2">
+                        <i class="bi bi-filter me-2"></i>
+                        Filter
+                    </div>
+                    <select class="form-select me-3 w-50" v-model="category" @change="loadProducts(reloadPage = true)">
+                        <option value="all"> All </option>
+                        <option v-for="category in categories" :key="category" :value="category"> {{ category }}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <div class="col d-flex align-items-center justify-content-end">
+                <label for="per-page" class="me-2"> <small> Show </small></label>
+                <input v-model="limit" type="number" id="per-page" @change="loadProducts(reloadPage = true)"
+                    class="form-control me-4" style="width: 5rem" min=4>
+                <div class="me-2"> <small>Sort</small></div>
+                <select v-model="sortOption" class="form-select w-50" aria-label="Default select example"
+                    @change="loadProducts(resetPage = true)">
+                    <option value="price_desc">Highest Price First </option>
+                    <option value="price_asc">Lowest Price First</option>
+                </select>
+            </div>
+        </div>
+
+        <div v-if="products.length > 0" class="row p-4">
+            <div v-for="product in products" :key="product._id" class="col-md-3 col-6 mb-4">
+                <div class="card">
+                    <router-link :to="{ name: 'ProductDetailView', params: { id: product._id } }">
+                        <img :src="product.image_path" class="card-img-top" style="height: 300px;" />
+                    </router-link>
+                    <div class="card-body">
+                        <router-link :to="{ name: 'ProductDetailView', params: { id: product._id } }"
+                            class="text-decoration-none text-dark fw-bold">
+                            {{ product.name }}
+                        </router-link>
+                        <p class="card-text"> {{ product.category }} </p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h6 class="fw-bolder"> ${{ product.price }}</h6>
+                            <button class="btn border-black" @click="addToCart(product)">Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-else class="d-flex justify-content-center p-4">
+            <div>
+                <span class="text-secondary">This category has no products...</span>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                    <li class="page-item text-black" @click="handleNavigate" :class="{ disabled: currentPage == 1 }"><a
+                            class="page-link text-black bg-tint" href="#"><span aria-hidden="true">&laquo;</span></a>
+                    </li>
+                    <li v-for="page in pageNumb" :key="page" class="page-item text-black" @click="handleNavigate"><a
+                            class="page-link text-black bg-tint" href="#"
+                            :class="{ 'current-active': currentPage == page }"> {{ page }} </a></li>
+                    <li class="page-item text-black" @click="handleNavigate"
+                        :class="{ disabled: currentPage == pageNumb }"><a class="page-link text-black bg-tint"
+                            href="#"><span aria-hidden="true">&raquo;</span></a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue'
+import axios from 'axios'
+
+export default {
+    name: 'ShopView',
+    components: {
+        Footer
+    },
+    data() {
+        return {
+            products: [],
+            offset: 0,
+            categories: [],
+            total: 0,
+            currentPage: 1,
+            // Filters & Sort
+            limit: 8,
+            category: 'all',
+            sortOption: 'price_asc',
+        }
+    },
+    computed: {
+        pageNumb() {
+            return Math.ceil(this.total / this.limit)
+        }
+    },
+    methods: {
+        async loadProducts(resetPage = false) {
+            if (resetPage) {
+                this.currentPage = 1
+                this.offset = 0
+            }
+            try {
+                let response = null
+                const limit = parseInt(this.limit) || 8
+                if (this.category !== 'all') {
+                    response = await axios.get(`/products?category=${this.category.toLowerCase()}&limit=${limit}&offset=${this.offset}&sort=${this.sortOption}`)
+                }
+                else {
+                    response = await axios.get(`/products?limit=${limit}&offset=${this.offset}&sort=${this.sortOption}`)
+                }
+                const data = response.data.data
+                this.products = data.products
+                this.total = data.total
+            }
+            catch (err) {
+                alert(err.message)
+            }
+        },
+        async loadCategories() {
+            try {
+                const response = await axios.get('/categories')
+                const data = response.data
+                this.categories = data.categories.map(category => category.name[0].toUpperCase() + category.name.slice(1))
+            }
+            catch (err) { alert(err.message) }
+        },
+        async handleNavigate(e) {
+            const page = e.target.innerText
+            if (page === '«') {
+                if (this.currentPage == 1) { return }
+                this.offset -= this.limit
+                this.currentPage--
+            }
+            else if (page === '»') {
+                if (this.currentPage == this.pageNumb) { return }
+                this.offset += this.limit
+                this.currentPage++
+            }
+            else {
+                this.offset = (page - 1) * this.limit
+                this.currentPage = page
+            }
+            this.loadProducts()
+        },
+        addToCart(product) {
+            this.$store.dispatch('addToCart', product)
+        }
+    },
+    mounted() {
+        console.log('Route params:', this.$route.params.id);
+        this.loadProducts()
+        this.loadCategories()
+
+    }
+}
+</script>
+
+<style scoped>
+.current-active {
+    color: white !important;
+    background-color: rgb(172, 148, 58);
+}
+</style>
---
+++ client/index.js
@@ -0,0 +1,133 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import { jwtDecode } from 'jwt-decode'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import ErrorView from '../views/ErrorView.vue'
+import CategoryManagementView from '../views/CategoryManagementView.vue'
+import ProductManagementView from '../views/ProductManagementView.vue'
+import CartView from '../views/CartView.vue'
+import CheckoutView from '../views/CheckoutView.vue'
+import StatisticView from '../views/StatisticsView.vue'
+import ShopView from '../views/ShopView.vue'
+import SearchView from '../views/SearchView.vue'
+import MyOrdersView from '../views/MyOrdersView.vue'
+import OrdersManagementView from '../views/OrdersManagementView.vue'
+import ProductDetailView from '@/views/ProductDetailView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: ProfileView,
+    meta: { requiresAuth: true } // [NOTE]: mark protected routes
+  },
+  
+  {
+    path: '/admin/category',
+    name: 'CategoryManagement',
+    component: CategoryManagementView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/admin/product',
+    name: 'ProductManagement',
+    component: ProductManagementView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/admin/statistic',
+    name: 'Statistic',
+    component: StatisticView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: CartView
+  },
+  {
+    path: '/checkout',
+    name: 'Checkout',
+    component: CheckoutView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/shop',
+    name: 'Shop',
+    component: ShopView
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: SearchView
+  },
+  {
+    path: '/product/:id',
+    name: 'ProductDetailView',
+    component: ProductDetailView,
+    props: true,
+  },
+  {
+    path: '/order',
+    name: 'MyOrders',
+    component: MyOrdersView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/admin/order',
+    name: 'OrdersManagement',
+    component: OrdersManagementView,
+    meta: { requiresAuth: true }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+// Setup routes protect (navigation guard)
+router.beforeEach((to, from) => {
+  const isRequiredAuth = to.meta.requiresAuth || false
+
+  if (!isRequiredAuth) {
+    return true
+  }
+  const token = localStorage.getItem('jwt')
+  if (!token) {
+    return { name: 'Login' }
+  }
+  const decoded = jwtDecode(token)
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    return { name: 'Login' }
+  }
+  return true
+})
+
+export default router
